aeae77800cb33c07ef64c616d1cb1a81ca04a7a0
wrryu09 <rryu09@ewhain.net>
2024-05-18 02:53:01 +0900

    init: 버튼 스타일링 예시

--- src/pages/APage/APage.jsx
+++ src/pages/APage/APage.jsx
@@ -1,9 +1,11 @@
+import Button from "../../components/Button/Button";
 import APageComponent from "./APageComponent/APageComponent";
 
 const APage = () => {
   return (
     <div>
       <APageComponent />
+      <Button />
     </div>
   );
 };
