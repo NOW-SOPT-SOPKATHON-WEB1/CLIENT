f865e97a5ba893c8c69d49adc30d857474f45d11
Jieun Lee <najee0610@naver.com>
2024-05-19 04:33:39 +0900

    feat: 게임 알림메세지 팝업 생성

---
+++ src/components/modal/IntroModalContent.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+import ModalCheckBtn from '../common/ModalCheckBtn';
+import WhiteModal from '../common/WhiteModal';
+
+const IntroModalContent = () => {
+  const [isOpenModal, setOpenModal] = useState(true);
+
+  const onClickToggleModal = () => {
+    setOpenModal(false);
+  };
+  return (
+    <>
+      {isOpenModal && (
+        <WhiteModal>
+          <TitleWrapper>
+            <p>
+              멜팅은 한 명의 휴대폰으로 진행됩니다. <br />
+              확인 버튼을 누르신 후, 다른 분들은 휴대폰을 내려놔 주세요!
+            </p>
+          </TitleWrapper>
+
+          <ModalCheckBtn type='button' onClick={onClickToggleModal}>
+            확인
+          </ModalCheckBtn>
+        </WhiteModal>
+      )}
+    </>
+  );
+};
+
+export default IntroModalContent;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  width: 28rem;
+  height: 7.2rem;
+  position: relative;
+  top: 3rem;
+  margin-bottom: 8rem;
+
+  p {
+    font-size: 1.6rem;
+    color: #525252;
+  }
+`;
