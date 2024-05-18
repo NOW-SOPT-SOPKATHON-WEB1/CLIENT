82d92424a65bceefa5fcd7949ce72c58cb9608b8
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-05-19 01:11:19 +0900

    fix: button 위치 조정

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,14 +1,8 @@
 import styled from '@emotion/styled';
 
-const Wrapper = styled.div`
-  position: relative;
-  /* height: 100%; */
-  width: 100%;
-`;
-
 const ButtonContainer = styled.button`
   position: absolute;
-  bottom: 0;
+  bottom: 1rem;
   left: 50%;
   transform: translateX(-50%);
 
@@ -29,11 +23,7 @@ const ButtonContainer = styled.button`
 `;
 
 const Button = ({ children }) => {
-  return (
-    <Wrapper>
-      <ButtonContainer>{children}</ButtonContainer>
-    </Wrapper>
-  );
+  return <ButtonContainer>{children}</ButtonContainer>;
 };
 
 export default Button;
