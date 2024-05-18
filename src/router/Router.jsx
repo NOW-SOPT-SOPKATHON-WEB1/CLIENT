13426287b81ca1fd215917d19a932ea7cde76c6a
Choi Minjune <minjune0807@gmail.com>
2024-05-19 00:50:27 +0900

    feat: 메인 페이지 초기 퍼블리싱

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,15 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import APage from "../pages/APage/APage";
-import BPage from "../pages/BPage/BPage";
-import CPage from "../pages/CPage/CPage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import APage from '../pages/APage/APage';
+import BPage from '../pages/BPage/BPage';
+import CPage from '../pages/CPage/CPage';
+import DPage from '../pages/DPage/DPage';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<APage />}></Route>
-        <Route path="/bpage" element={<BPage />}></Route>
-        <Route path="/cpage" element={<CPage />}></Route>
+        <Route path='/' element={<APage />}></Route>
+        <Route path='/bpage' element={<BPage />}></Route>
+        <Route path='/cpage' element={<CPage />}></Route>
+        <Route path='/dpage' element={<DPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
