312afd48506bfde3288e0aa06d68ca06623b4b42
Jieun Lee <najee0610@naver.com>
2024-05-19 04:32:36 +0900

    style: modal 공통 확인 버튼 색상 수정

--- src/components/common/ModalCheckBtn.jsx
+++ src/components/common/ModalCheckBtn.jsx
@@ -11,7 +11,7 @@ const CheckBtn = styled.button`
   width: 30.2rem;
   height: 5.3rem;
   color: white;
-  background-color: #787878;
+  background-color: #6c9eff;
   border: none;
   border-radius: 0.8rem;
   font-size: 2rem;
