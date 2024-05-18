8df0a86f122698f23b47d1cff91fee632c0be652
Jieun Lee <najee0610@naver.com>
2024-05-19 04:12:19 +0900

    style: 방 만들기 모달 수정

--- src/components/modal/CreateRoomModalContent.jsx
+++ src/components/modal/CreateRoomModalContent.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Modal from '../common/Modal';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import styled from '@emotion/styled';
-import ModalCheckBtn from '../common/ModalCheckBtn';
+import codeBG from '../../assets/modal/codeBG.svg';
 
 const CreateRoomModalContent = () => {
   const [value, setValue] = useState('000000');
@@ -17,27 +17,26 @@ const CreateRoomModalContent = () => {
       {isOpenModal && (
         <Modal>
           <TitleWrapper>
-            <h5>방이 생성되었습니다</h5>
-            <CloseBtn onClick={onClickToggleModal}>X</CloseBtn>
+            <h5>얼음방이 생성되었습니다</h5>
           </TitleWrapper>
 
-          <LabelWrapper>
-            <p>초대 코드</p>
-            <CopyToClipboard
-              text={value}
-              onCopy={() => alert('방 코드가 복사되었습니다.')}
-            >
-              <CopyBtn>복사하기</CopyBtn>
-            </CopyToClipboard>
-          </LabelWrapper>
           <NumberDisplay>
             {value.split('').map((digit, index) => (
-              <NumberWrapper key={index}>{digit}</NumberWrapper>
+              <NumberContainer key={index}>
+                <NumberImage src={codeBG} alt='code bg' />
+                <NumberText>{digit}</NumberText>
+              </NumberContainer>
             ))}
           </NumberDisplay>
-          <ModalCheckBtn type='button' onClick={onClickToggleModal}>
-            확인
-          </ModalCheckBtn>
+
+          <CopyToClipboard
+            text={value}
+            onCopy={() => alert('방 코드가 복사되었습니다.')}
+          >
+            <CopyBtn onClick={onClickToggleModal} type='button'>
+              초대코드 복사하기
+            </CopyBtn>
+          </CopyToClipboard>
         </Modal>
       )}
     </>
@@ -49,40 +48,50 @@ const TitleWrapper = styled.div`
   align-items: center;
   justify-content: space-between;
   gap: 10rem;
-`;
-const LabelWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 10rem;
-  justify-content: space-between;
+  font-size: 1.8rem;
+  color: #fff;
+
+  h5 {
+    font-size: 1.8rem;
+  }
 `;
 
 const NumberDisplay = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
   margin: 1rem 0 2rem 0;
+  width: 23rem;
+  height: 4.5rem;
 `;
-const NumberWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 0.5rem;
-  border-radius: 1rem;
-  width: 2.7rem;
-  height: 2.9rem;
-  margin: 0.2rem;
-  background-color: lightgray;
-`;
-const CloseBtn = styled.button`
-  width: 1.3rem;
-  height: 2.2rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: transparent;
-  border: none;
-`;
+
 const CopyBtn = styled.button`
+  width: 30.2rem;
+  height: 5.3rem;
+  color: white;
+  background-color: #6c9eff;
   border: none;
+  border-radius: 0.8rem;
+  font-size: 2rem;
+  font-weight: bold;
+`;
+
+const NumberContainer = styled.div`
+  position: relative;
+`;
+
+const NumberImage = styled.img`
+  margin-right: 0.5rem;
+`;
+
+const NumberText = styled.span`
+  position: absolute;
+  top: 50%;
+  left: 45%;
+  transform: translate(-50%, -50%);
+  color: #525252;
+  font-size: 1.8rem;
+  font-weight: bolder;
 `;
 
 export default CreateRoomModalContent;
