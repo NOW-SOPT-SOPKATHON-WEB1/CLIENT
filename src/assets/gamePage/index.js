aa315332dcdac7330ba38e4b86ca0e47d0deff86
Jieun Lee <najee0610@naver.com>
2024-05-19 06:45:41 +0900

    feat: svg assets 추가

---
+++ src/assets/gamePage/index.js
@@ -0,0 +1,11 @@
+import malpungson from './malpungson.svg';
+import meltinglogo from './melting_logo.svg';
+import penguin from './penguin.svg';
+
+const svgitems = {
+  malpungson,
+  meltinglogo,
+  penguin,
+};
+
+export default svgitems;
